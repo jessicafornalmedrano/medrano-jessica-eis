8ac5cd5636614fd811a208f7c950e01c3c1f1c97
unknown <jessicamedrano1203@gmail.com>
2024-05-27 15:09:14 +0800

    hello

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from users import Users
+from employees import Employee
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/check-user', methods=['POST'])
+def check_user():
+   username = request.form["username"]
+   password = request.form["password"]
+
+   result = Users.check_user(username, password)
+   
+   if result:
+    return redirect('/employee-list')
+   else:
+    return render_template('index.html')
+
+@app.route('/employee-list')
+def employee_list():
+    employees = Employee.get_all()
+    return render_template('employee_list.html', employees=employees)
+
+if __name__ == '__main__':
+    app.run()
+
+    
